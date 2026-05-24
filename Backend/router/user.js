dc1dfe7fe8e4282b22faa0779dbfbb5a6208d233
dolishaa29 <dolishagandhi@gmail.com>
2026-05-25 01:29:21 +0530

    neww

--- Backend/router/user.js
+++ Backend/router/user.js
@@ -3,10 +3,12 @@ const express = require("express");
 let router = express.Router();
 const { dashboard, profile, updateProfile,register,login } = require("../service/user");
 let auth=require("../middleware/user");
+const upload = require("../middleware/multer"); 
+const cloudinary = require("../config/cloudinary");
 
-router.post("/register",register);
+router.post("/register", upload.single("image"), register);
 router.post("/login",login);
 router.get('/dashboard',auth,dashboard);
 router.get('/profile',auth,profile);
-router.post('/updateprofile',auth,updateProfile);
+router.post('/updateprofile',auth,upload.single("image"),updateProfile);
 module.exports=router;
