fd6aed0ccd32805ea6a6f441c21003aa38538dc5
dolishaa29 <dolishagandhi@gmail.com>
2026-05-25 12:32:17 +0530

    neww

--- Backend/router/blog.js
+++ Backend/router/blog.js
@@ -1,12 +1,25 @@
-let blogmodel=require("../model/blog");
 const express = require("express");
-let router = express.Router();
-let auth=require("../middleware/user");
+const router = express.Router();
+const auth = require("../middleware/user");
 const upload = require("../middleware/multer");
-const cloudinary = require("../config/cloudinary");
+
+const { 
+  addBlog, 
+  viewUserBlogs, 
+  editBlog, 
+  deleteBlog, 
+  singleUserBlog, 
+  exploreAllBlogs, 
+  exploreSingleBlog 
+} = require("../service/blog"); 
 
 
-router.post("/addblog");
+router.post("/addBlog", auth, upload.single("image"), addBlog);
+router.get("/viewUserBlogs", auth, viewUserBlogs);
+router.put("/editBlog/:id", auth, upload.single("image"), editBlog);
+router.delete("/deleteBlog/:id", auth, deleteBlog);
+router.get("/singleUserBlog/:id", auth, singleUserBlog);
+router.get("/exploreAllBlogs", exploreAllBlogs);
+router.get("/exploreSingleBlog/:id", exploreSingleBlog);
 
-
-module.exports=router;
+module.exports = router;
